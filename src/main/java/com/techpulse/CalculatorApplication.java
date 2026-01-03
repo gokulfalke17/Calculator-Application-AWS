d949547c86c5db8c174989f8ef2a71dd789fd809
Gokul Falke <145555165+gokulfalke17@users.noreply.github.com>
2026-01-03 14:46:15 +0530

    Feat :: After Adding Testcases for calculation and in deploy.yml file i added stage that is Run Maven Tests

--- src/main/java/com/techpulse/CalculatorApplication.java
+++ src/main/java/com/techpulse/CalculatorApplication.java
@@ -3,7 +3,7 @@ package com.techpulse;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+//@SpringBootApplication
 public class CalculatorApplication {
 
 	public static void main(String[] args) {
